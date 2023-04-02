b357f9c5aef15ef02a53eebf746528c45ca88e1e
Kayo Elias <contatodevkayo@gmail.com>
2023-04-02 16:36:28 -0300

    feat : global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+* {
+  margin:  0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
+body {
+  background: #fdfdfd;
+}
+
+`;
